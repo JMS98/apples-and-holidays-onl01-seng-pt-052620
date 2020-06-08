58038d345c35e1cba2ee8cd81e351e1d5f367960
Jonathan Snipe <jon.mike919@gmail.com>
2020-06-08 00:57:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,6 @@ holiday_hash.each do [season,holiday]
     puts "  #{event.to_s.split("_").map {|x|x.capitalize}.join("")}:#{supplies.join(",")}"
     end
   end
-end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
